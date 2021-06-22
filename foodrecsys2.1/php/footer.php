d28bd31d61756941b43575f9b0b80d56550a11ec
leo-tarta <leo.tartarello@gmail.com>
2021-06-22 17:02:29 +0200

    Fix landing page

--- foodrecsys2.1/php/footer.php
+++ foodrecsys2.1/php/footer.php
@@ -5,9 +5,9 @@
                 <span> &copy; 2020 <a href="http://www.di.uniba.it/~swap/" target="_blank"> SWAP Research Group </a>,</span>
                 <span>&nbsp;<a href="privacy.html" target="_blank">Privacy Policy </a></span>
             </div>
-            <div class="col-md-6 d-flex flex-row-reverse  ">
+            <!-- <div class="col-md-6 d-flex flex-row-reverse  ">
                 <span> &copy; Developed by <a href="https://github.com/andmon97" target="_blank">Andrea Montemurro</a></span>
-            </div>
+            </div> -->
         </div>
     </div>
 </footer>
